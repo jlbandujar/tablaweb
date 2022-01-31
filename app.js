0ecccb70068d09a97356f65c835062597eab0c84
jlbandujar@gmail.com <jlbandujar@gmail.com>
2022-01-31 16:53:47 +0100

    Primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+app.use(express.static('public'));
+app.get('/tabla', function (req, res) {
+  let num = req.query.num;
+  let tabla="<table>";
+  for ( i=1; i<=10; i++){
+        tabla+=`<tr><td>${i} por ${num} = ${i*num}</td></tr>`;
+  }
+  tabla+='</table>';
+  res.send(tabla);
+})
+const puerto=3000
+app.listen(puerto, ()=>console.log('Escuchando el puerto', puerto))
